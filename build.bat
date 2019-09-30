310e2b78e86d4ca7b3c4ef37a8c6c663606d2fe3
Alex Walters <1781587+tritium21@users.noreply.github.com>
2019-09-30 03:46:05 -0400

    Requirements + no more pyinstaller
    ...for now

--- build.bat
+++
@@ -1,5 +0,0 @@
-rmdir /q /s dist
-if not exist "%~dp0build_env\" py -3.7 -m venv build_env
-build_env\scripts\python -m pip install --upgrade pip wheel setuptools
-build_env\scripts\python -m pip install --upgrade -r requirements_build.txt
-build_env\scripts\pyinstaller run_tritki.spec
--- run_tritki.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-import tritki.main
-
-if __name__ == '__main__':
-    sys.exit(tritki.main.main(sys.argv))
